b060d4036576e620261b14550359883b73dc4753
sam-moat <sam_moat@hotmail.com>
2017-09-26 19:36:19 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,54 +4,47 @@ var map;
 var markers = [];
 
 var Place = function(data) {
-  this.title = data.title;
-  this.location = data.location;
+    this.title = data.title;
+    this.location = data.location;
 };
 
 function AppViewModel() {
-  var self = this;
-  this.query = ko.observable('');
-  //creates observable array
-  this.placeList = ko.observableArray([]);
-  //pushes each location into obervable array
-  locations.forEach(function(place){
-    self.placeList.push(new Place(place));
-  });
+    var self = this;
+    this.query = ko.observable('');
+    //creates observable array
+    this.placeList = ko.observableArray([]);
+    //pushes each location into obervable array
+    locations.forEach(function(place) {
+        self.placeList.push(new Place(place));
+    });
 
-//animates the selected marker
-  this.clickPlace = function(place){
-    //console.log(place.marker.title);
-    google.maps.event.trigger(place.marker, 'click');
-  };
+    //animates the selected marker
+    this.clickPlace = function(place) {
+        //console.log(place.marker.title);
+        google.maps.event.trigger(place.marker, 'click');
+    };
 
-//filters the locations and markers using the input from the searchbar
-  this.filteredItems = ko.computed(function() {
-    var filter = self.query().toLowerCase();
-    /*console.log(filter);
-     //if (!filter) {
-       //console.log(self.placeList());
-      //  return self.placeList();
-    //} else { */
+    //filters the locations and markers using the input from the searchbar
+    this.filteredItems = ko.computed(function() {
+        var filter = self.query().toLowerCase();
+        /*console.log(filter);
+         //if (!filter) {
+           //console.log(self.placeList());
+          //  return self.placeList();
+        //} else { */
         return ko.utils.arrayFilter(self.placeList(), function(place) {
-          var match = place.title.toLowerCase().indexOf(filter) !== -1;
-          if(place.marker) {
-            place.marker.setVisible(match);
-          }
+            var match = place.title.toLowerCase().indexOf(filter) !== -1;
+            if (place.marker) {
+                place.marker.setVisible(match);
+            }
             return match;
         });
 
 
-  //  }
-}, self);
+        //  }
+    }, self);
 
 
-// TODO: Need to hide the marker infowindow after someone has changed their search
-
-  // filtering of the locations (focus on the list view items first before synchronizing with the markers)
-  // http://knockoutjs.com/documentation/computedObservables.html
-  // https://opensoul.org/2011/06/23/live-search-with-knockoutjs/
-  // http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
 
 }
 
@@ -66,6 +59,7 @@ function initMap() {
             lat: -40.900557,
             lng: 174.885971
         },
+        //controls the styles and controls for the map
         zoom: 5,
         styles: styles,
         panControl: true,
@@ -84,61 +78,88 @@ function initMap() {
 
     // The following group uses the location array to create an array of markers on initialize.
     for (var i = 0; i < locations.length; i++) {
-      // Get the position from the location array.
-      var position = locations[i].location;
-      var title = locations[i].title;
-      // Create a marker per location, and put into markers array.
-      var marker = new google.maps.Marker({
-        map: map,
-        position: position,
-        title: title,
-        animation: google.maps.Animation.DROP,
-        id: i
-      });
-      // Push the marker to our array of markers.
-      markers.push(marker);
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].title;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
 
-      //adds marker information to the placeList array
-      appViewModel.placeList()[i].marker = marker;
+        //adds marker information to the placeList array
+        appViewModel.placeList()[i].marker = marker;
 
-      // Create an onclick event to open an infowindow at each marker.
-      marker.addListener('click', function() {
-        populateInfoWindow(this, largeInfowindow);
-        stopAnimation();
-        this.setAnimation(google.maps.Animation.BOUNCE);
-        //setTimeout(function(){this.marker.setAnimation(null); }, 1400);
-      });
+        // Create an onclick event to open an infowindow at each marker.
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+            stopAnimation();
+            this.setAnimation(google.maps.Animation.BOUNCE);
+            //setTimeout(function(){this.marker.setAnimation(null); }, 1400);
+        });
 
 
 
-      bounds.extend(markers[i].position);
+        bounds.extend(markers[i].position);
     }
     // Extend the boundaries of the map for each marker
     map.fitBounds(bounds);
-  }
+}
 
-  // This function populates the infowindow when the marker is clicked. We'll only allow
-  // one infowindow which will open at the marker that is clicked, and populate based
-  // on that markers position.
-  function populateInfoWindow(marker, infowindow) {
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
-      infowindow.marker = marker;
-      infowindow.setContent('<div>' + marker.title + '</div>');
-      infowindow.open(map, marker);
-      // Make sure the marker property is cleared if the infowindow is closed.
-      infowindow.addListener('closeclick',function(){
-        infowindow.setMarker = null;
-      });
+        //New York Times API request
+        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + marker.title + "&sort=newest&api-key=16cfed740ab04b5aa04a888e07ff7144";
+        //console.log(url);
+
+        //error handling for New York Times API request
+        $.ajaxSetup({
+            "error": function() {
+                infowindow.setContent('News article could not be loaded');
+            }
+        });
+        //API request for New York Times
+        $.getJSON(url, function(data) {
+            //populates the infowindow with API response
+            articles = data.response.docs;
+            for (var i = 0; i < articles.length; i++) {
+                var article = articles[i];
+                infowindow.setContent('<h3>Latest from the New York Times about ' + marker.title + ':</h3>' + '<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+            }
+
+
+        });
+        /*.error(function(e) {
+            infowindow.setContent('News article could not be loaded');
+        }); */
+
+
+        infowindow.marker = marker;
+        //infowindow.setContent('<div>' + marker.title + '</div>');
+
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function() {
+            infowindow.setMarker = null;
+        });
     }
-  }
+}
 
 //loops through markers array and stops animations
-  stopAnimation = function(){
+stopAnimation = function() {
     for (var i = 0; i < markers.length; i++) {
-      markers[i].setAnimation(null);
+        markers[i].setAnimation(null);
     }
-  };
+};
 
 /*
 //replacement for stringStartsWith which was removed from KO. credit: https://stackoverflow.com/questions/28042344/filter-using-knockoutjs
@@ -150,7 +171,7 @@ function initMap() {
 };
 */
 
-  // Activates knockout.js
+// Activates knockout.js
 var appViewModel = new AppViewModel();
 
 ko.applyBindings(appViewModel);
